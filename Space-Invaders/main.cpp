106eb89c6a4d2643cfbf0f93d97fca89453888d0
pranaysj <pranaysj3@gmail.com>
2024-05-27 15:55:32 +0530

    Create Render window
    1.Created render window with videomode class to define the window size.
     2.Created game loop

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,6 +1,30 @@
+#include<SFML/Graphics.hpp>
+using namespace sf;
 
 int main()
 {
-    //Feature-1-SFML
+    VideoMode* videoMode = new VideoMode(800,800);
+    RenderWindow* window = new RenderWindow(*videoMode,"SFML",Style::Default);
+    //window->setFramerateLimit(60);
+    while (window->isOpen())
+    {
+        window->setPosition(Vector2i(0,0));
+        Event event;
+        while (window->pollEvent(event))
+        {
+            if (event.type == Event::Closed) {
+                window->close();
+            }
+        }
+
+
+        window->clear(Color::Green);
+
+
+        window->display();
+    }
+
+   
+    
     return 0;
 }
