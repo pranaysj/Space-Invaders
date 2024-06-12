e8bc2a66f4f0e776a2db8c76a13b4c6d91427386
pranaysj <pranaysj3@gmail.com>
2024-06-12 16:12:22 +0530

    Update main.cpp
    Created game loop, class and member variables and access the member variable in main function.

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,6 +1,44 @@
+#include<SFML/Graphics.hpp>
+#include<iostream>
 
-int main()
-{
-    //new
+using namespace sf;
+using namespace std;
+
+class Player {
+    public :
+        Texture player_texture;
+        Sprite player_sprite;
+        int health = 3;
+        Vector2f position = Vector2f(200.0f, 100.0f);
+        int movement_speed = 5;
+        int player_score = 0;
+};
+
+int main() {
+
+    // Define the video mode (dimensions)
+    VideoMode* videoMode = new VideoMode(800, 600);
+
+    // Create a window object with specific dimensions and a title
+    RenderWindow* window= new RenderWindow(*videoMode, "SFML Window");
+
+    while (window->isOpen()) {
+        sf::Event event;
+        while (window->pollEvent(event)) {
+            // Check for window closure
+            if (event.type == Event::Closed)
+                window->close();
+        }
+
+        // Clear the window
+        window->clear(Color::Blue);
+
+        Player player;
+        cout << player.movement_speed << "\n";
+
+        // Display whatever you draw
+        window->display();
+    }
+
     return 0;
 }
